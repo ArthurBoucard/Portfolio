dd9655fe33d89deb1e40c3bad25f84d6aa1fbbdf
ArthurBoucard <arthur.boucard@hotmail.fr>
2024-02-21 21:12:59 +0100

    added project type

--- pages/projects.vue
+++ pages/projects.vue
@@ -66,7 +66,7 @@
           <span class="flex text-sm my-3">
             <h3 v-if="index == null" class="text-purplefy font-fira_bold mr-3">{{ project.title }}</h3>
             <h3 v-else class="text-purplefy font-fira_bold mr-3">{{ project.title }}</h3>
-            <!-- <h4 class="font-fira_retina text-menu-text"> // {{ project.tech }}</h4> -->
+            <h4 class="font-fira_retina text-menu-text"> // {{ project.type }}</h4>
           </span>
 
           <!-- info -->
